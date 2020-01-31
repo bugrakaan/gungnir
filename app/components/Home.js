3ba7db37da4ec9f79138521414105138053c38dc
Kaan <bugrakaan@gmail.com>
2020-01-31 14:57:49 +0300

    get library items from utils method

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import XMLParser from 'fast-xml-parser';
-
-import path from 'path';
-import FS from 'fs';
 import Library from './Library';
 
+import { getLibraryItems } from '../utils';
+
 export default class Home extends Component {
-
-  get libraryItems() {
-    const SettingsXML = FS.readFileSync(path.resolve(__dirname, './mockdata/cemusettings.xml')).toString();
-    const Settings = XMLParser.parse(SettingsXML);
-    const { content: { GameCache: { Entry: gameList } = {} } = {} } = Settings
-
-    return gameList;
-  }
-
   render() {
     return (
       <div className="App">
         <div className="Header" />
-        <Library items={this.libraryItems} />
+        <Library items={getLibraryItems()} />
         <div className="Footer" />
       </div>
     );
